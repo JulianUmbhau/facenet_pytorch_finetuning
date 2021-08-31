3a6a85c834586fc568cf1ce0ea6ac76051b2121b
Julian Johannes Umbhau Jensen <jensen_303@hotmail.com>
2021-09-01 00:33:13 +0200

    face recognition stream using facenet_recognitino template. Still missing person face recognition

--- finetuning_script.py
+++ finetuning_script.py
@@ -1,3 +1,5 @@
+# Finetuning script on putin/julian images
+
 # %%
 from facenet_pytorch import MTCNN, InceptionResnetV1, fixed_image_standardization, training
 import torch
@@ -30,13 +32,14 @@ mtcnn = MTCNN(
 )
 
 #%%
-
 dataset = datasets.ImageFolder(data_dir, transform=transforms.Resize((512, 512)))
 dataset.samples = [
     (p, p.replace(data_dir, data_dir + '_cropped'))
         for p, _ in dataset.samples
 ]
-        
+
+# %%
+
 loader = DataLoader(
     dataset,
     num_workers=workers,
@@ -52,4 +55,121 @@ for i, (x, y) in enumerate(loader):
 del mtcnn
 
 #%%
+resnet = InceptionResnetV1(
+    classify=True,
+    pretrained='vggface2',
+    num_classes=len(dataset.class_to_idx)
+).to(device)
 
+#%%
+optimizer = optim.Adam(resnet.parameters(), lr=0.001)
+scheduler = MultiStepLR(optimizer, [5, 10])
+
+trans = transforms.Compose([
+    np.float32,
+    transforms.ToTensor(),
+    fixed_image_standardization
+])
+dataset = datasets.ImageFolder(data_dir + '_cropped', transform=trans)
+img_inds = np.arange(len(dataset))
+np.random.shuffle(img_inds)
+train_inds = img_inds[:int(0.8 * len(img_inds))]
+val_inds = img_inds[int(0.8 * len(img_inds)):]
+
+train_loader = DataLoader(
+    dataset,
+    num_workers=workers,
+    batch_size=batch_size,
+    sampler=SubsetRandomSampler(train_inds)
+)
+val_loader = DataLoader(
+    dataset,
+    num_workers=workers,
+    batch_size=batch_size,
+    sampler=SubsetRandomSampler(val_inds)
+)
+#%%
+loss_fn = torch.nn.CrossEntropyLoss()
+metrics = {
+    'fps': training.BatchTimer(),
+    'acc': training.accuracy
+}
+
+#%%
+
+writer = SummaryWriter()
+writer.iteration, writer.interval = 0, 10
+
+print('\n\nInitial')
+print('-' * 10)
+resnet.eval()
+training.pass_epoch(
+    resnet, loss_fn, val_loader,
+    batch_metrics=metrics, show_running=True, device=device,
+    writer=writer
+)
+
+for epoch in range(epochs):
+    print('\nEpoch {}/{}'.format(epoch + 1, epochs))
+    print('-' * 10)
+
+    resnet.train()
+    training.pass_epoch(
+        resnet, loss_fn, train_loader, optimizer, scheduler,
+        batch_metrics=metrics, show_running=True, device=device,
+        writer=writer
+    )
+
+    resnet.eval()
+    training.pass_epoch(
+        resnet, loss_fn, val_loader,
+        batch_metrics=metrics, show_running=True, device=device,
+        writer=writer
+    )
+
+writer.close()
+
+# %% 
+
+# Saving and loading model
+# Specify a path to save to
+PATH = "model.pt"
+
+# Save
+torch.save(resnet.state_dict(), PATH)
+
+# Load
+device = torch.device('cpu')
+
+model = InceptionResnetV1(
+    classify=True,
+    pretrained='vggface2',
+    num_classes=len(dataset.class_to_idx)
+)
+
+model.load_state_dict(torch.load(PATH, map_location=device))
+# %%
+
+# test on own picture
+
+from PIL import Image
+image_path = "./2021-08-30-221618.jpg"
+img = Image.open(image_path)
+
+trans = transforms.Compose([
+    np.float32,
+    transforms.ToTensor(),
+    fixed_image_standardization
+])
+
+trans = transforms.ToTensor()
+
+input = trans(img)
+
+input = input.unsqueeze(0)
+model.eval()
+
+output = model(input)
+model
+
+# %%
